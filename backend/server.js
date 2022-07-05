3380d929ee5599806cfb0eec45b41ea1a4834542
Jonah Halili <j0halili00@gmail.com>
2022-07-05 03:23:03 -0400

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const path = require("path");
+const { response } = require('express');
 
 const app = express();
 dotenv.config();
-
-app.get('/', (req, res) => {
-    res.send("API is running");
-});
+connectDB();
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
@@ -17,6 +17,20 @@ app.get('/api/notes/:id', (req, res) => {
     const note = notes.find((n) => n._id === req.params.id);
     res.send(note);
 });
+
+// ------------deployment--------------
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) => {
+        response.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send("API is running");
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
