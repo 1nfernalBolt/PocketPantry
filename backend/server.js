9f5e277f9fb81b5d33825968c948a350f08dc780
CoreyClements1 <106399487+CoreyClements1@users.noreply.github.com>
2022-07-05 21:42:13 -0400

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,13 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const path = require("path");
 const { response } = require('express');
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 dotenv.config();
 connectDB();
+app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
@@ -31,6 +34,10 @@ if (process.env.NODE_ENV === 'production') {
         res.send("API is running");
     });
 }
+
+app.use('/api/users', userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
