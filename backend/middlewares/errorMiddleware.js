a696293b161d07c58284e8eb7679b87e62c85a40
Jonah Halili <j0halili00@gmail.com>
2022-07-07 17:45:07 -0400

    api test

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const notFound = (req, res, next) => {
     const error = new Error(`Not found - ${req.originalUrl}`);
     res.status(404);
@@ -14,21 +13,4 @@ const errorHandler = (err, req, res, next) => {
     });
 };
 
-=======
-const notFound = (req, res, next) => {
-    const error = new Error(`Not found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-};
-
-const errorHandler = (err, req, res, next) => {
-    const statusCode = req.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === "production" ? null : err.stack,
-    });
-};
-
->>>>>>> fc07685652326de48e9b52bf696a2aaecf6ddd1f
 module.exports = { notFound, errorHandler };
