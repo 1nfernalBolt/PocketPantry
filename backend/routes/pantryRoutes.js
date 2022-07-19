41a7ff8a7431e3ebb19ac33f254a60e48a7d4961
Jonah Halili <j0halili00@gmail.com>
2022-07-19 15:38:02 -0400

    fixed repetitions

--- backend/routes/pantryRoutes.js
+++ backend/routes/pantryRoutes.js
@@ -11,15 +11,4 @@ router.route('/removeIngredient').post(protect, removeIngredient);
 router.route('/searchIngredientByName').post(protect, searchIngredientByName);
 router.route('/getPantry').post(protect, getPantry);
 
-const express = require('express');
-const { clearPantry, addIngredient, removeIngredient, searchIngredientByName, getPantry } = require('../controllers/pantryControllers');
-router = express.Router();
-
-// Routes for API's go here
-router.route('/clearPantry').post(clearPantry);
-router.route('/addIngredient').post(addIngredient);
-router.route('/removeIngredient').post(removeIngredient);
-router.route('/searchIngredientByName').post(searchIngredientByName);
-router.route('/getPantry').post(getPantry);
-
 module.exports = router;
--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -11,15 +11,4 @@ router.route('/removeRecipeById').post(protect, removeRecipeById);
 router.route('/searchRecipeByName').post(protect, searchRecipeByName);
 router.route('/getRecipes').post(protect, getRecipes);
 
-const express = require('express');
-const { addRecipe, removeRecipeById, searchRecipeByName, getRecipes, clearRecipes } = require('../controllers/recipeControllers');
-router = express.Router();
-
-// Routes for API's go here
-router.route('/clearRecipes').post(clearRecipes);
-router.route('/addRecipe').post(addRecipe);
-router.route('/removeRecipeById').post(removeRecipeById);
-router.route('/searchRecipeByName').post(searchRecipeByName);
-router.route('/getRecipes').post(getRecipes);
-
 module.exports = router;
