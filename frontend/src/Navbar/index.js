35691c8160af5ff081e07da990210e90bec49207
Betul <60045711+betulyazm@users.noreply.github.com>
2022-07-23 16:59:01 -0400

    Pantry, Recipe and Shopping cart buttons added

--- frontend/src/Navbar/index.js
+++ frontend/src/Navbar/index.js
@@ -1,30 +1,56 @@
-import React from 'react';
-
-import {Nav, NavLink, NavMenu, NavBtn, NavBtnLink, Bars} from "./NavbarEl";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import '../App.css'
+import { Nav, NavLink, NavMenu, NavBtn, NavBtnLink, Bars } from "./NavbarEl";
 import pocketPantryLogo from './images/pocketPantryLogo.png';
-
+import { logout } from '../actions/userActions';
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-  <> 
-   <Nav>
-   <Bars />
-    <NavLink to = "/login">
-        <img src= {pocketPantryLogo} alt = "pocketPantryLogo" height = "100" width = "100" />
-    </NavLink>
-    <NavMenu>
-    </NavMenu>
-    <NavMenu>
-    <NavBtn>
-          <NavBtnLink to='/login'>Sign In</NavBtnLink>
-    </NavBtn>
-    <NavBtn>
-          <NavBtnLink to='/register'>Register</NavBtnLink>
-    </NavBtn>
-    </NavMenu>
-   </Nav>
-   </>
-  )
+
+      const userLogin = useSelector((state) => state.userLogin);
+      const { userInfo } = userLogin;
+      const navigate = useNavigate();
+      const dispatch = useDispatch();
+
+
+      var class_name = (userInfo) ? "hidden" : "";
+      var class_name2 = (userInfo) ? "nav-links" : "hidden";
+      const logoutHandler = () => {
+            dispatch(logout());
+      };
+      return (
+            <>
+                  <Nav>
+                        <Bars />
+                        <NavLink to="/">
+                              <img src={pocketPantryLogo} alt="pocketPantryLogo" height="80" width="80" />
+                        </NavLink>
+                        <NavMenu>
+                        </NavMenu>
+                        <NavMenu>
+                              <NavBtn>
+                                    <NavBtnLink className={class_name2} to='/recipes'>Recipes</NavBtnLink>
+                              </NavBtn>
+                              <NavBtn>
+                                    <NavBtnLink className={class_name2} to='/pantry'>Pantry</NavBtnLink>
+                              </NavBtn>
+                              <NavBtn>
+                                    <NavBtnLink className={class_name2} to='/cart'>Shopping Cart</NavBtnLink>
+                              </NavBtn>
+                              <NavBtn>
+                                    <NavBtnLink className={class_name} to='/register'>Register</NavBtnLink>
+                              </NavBtn>
+                              <NavBtn>
+                                    <NavBtnLink className={class_name} to='/login'>Sign In</NavBtnLink>
+                              </NavBtn>
+                              <NavBtn>
+                                    <NavBtnLink to="/login" className={class_name2} onClick={logoutHandler}>Logout</NavBtnLink>
+                              </NavBtn>
+                        </NavMenu>
+                  </Nav>
+            </>
+      )
 }
 
 export default Navbar
