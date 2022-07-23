910d86647b1e127be7a55579ba4d2e100c95ede5
Betul <60045711+betulyazm@users.noreply.github.com>
2022-07-23 16:29:19 -0400

    added routes to pantry and recipe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,35 @@
 import React from "react";
-import {Routes, Route, useNavigate, Link} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import Login from "./pages/login";
 import Register from "./pages/register";
 import EmailConf from "./pages/emailConf";
-import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import { Parallax, ParallaxLayer} from '@react-spring/parallax';
-import white from './images/white.png';
-import pantry from './images/pantry.jpg';
-import pocketPantryLogo from './images/pocketPantryLogo.png';
+import { useState } from "react";
 import Navbar from "./Navbar";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter  as Router } from "react-router-dom";
 import useStyles from './styles';
+import Pantry from "./pages/pantry";
+import Recipes from "./pages/recipes";
 
+const App = () => {
 
-const App =  () => (
-    <>
-    <main>
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path='/' element={<Login/>} />
-                <Route path='/login' element={<Login/>} />
-                <Route path ='/register' element={<Register/>} />
-                <Route path='/emailConf' element={<EmailConf/>} />
-                <Route path='/forgotPass' element={<forgotPass/>} />
-            </Routes>
-        </Router>
-    </main>
-    </>
-);
+    return (
+        <div className="App">
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Register />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/emailConf' element={<EmailConf />} />
+                    <Route path="/pantry" element={<Pantry />} />
+                    <Route path="/recipes" element={<Recipes />} />
+                    <Route path='/forgotPass' element={<forgotPass />} />
+                </Routes>
+            </Router>
+        </div>
+    )
+}
 
 export default App;
